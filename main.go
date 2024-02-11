ce7caa426fc46ac17b767c0bc3f3a361d44210a8
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-02-11 20:31:20 -0300

    feat: v1

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Olá, mundo!")
+	http.HandleFunc("/upload-csv", uploadCSVHandler)
+	fmt.Println("Servidor rodando na porta 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func uploadCSVHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	r.ParseMultipartForm(32 << 20)
+
+	file, _, err := r.FormFile("arquivo")
+	if err != nil {
+		http.Error(w, "Erro ao obter o arquivo", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	reader.ReuseRecord = true
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			http.Error(w, "Erro ao ler o arquivo CSV", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println(record)
+	}
+
+	fmt.Fprintln(w, "Arquivo processado com sucesso")
 }
